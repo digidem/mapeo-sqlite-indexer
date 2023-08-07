a46beae2ae27f63edb0080f261d983b9df333b77
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-08-07 19:46:06 +0100

    fix!: rename columns to match new schema (#11)

--- test/scenarios.js
+++ test/scenarios.js
@@ -3,44 +3,86 @@ import test from 'tape'
 import { create, permute } from './utils.js'
 
 const docs = [
-  { id: 'A', version: '1', links: [] },
-  { id: 'A', version: '2', links: ['1'] },
-  { id: 'A', version: '3', links: ['1'] },
-  { id: 'A', version: '4', links: ['2', '3'] },
-  { id: 'A', version: '5', links: ['4'] },
-  { id: 'A', version: '6', links: ['4'] },
-  { id: 'A', version: '7', links: ['4'] },
-  { id: 'A', version: '8', links: ['5', '6'] },
+  { docId: 'A', versionId: '1', links: [], updatedAt: '' },
+  { docId: 'A', versionId: '2', links: ['1'], updatedAt: '' },
+  { docId: 'A', versionId: '3', links: ['1'], updatedAt: '' },
+  { docId: 'A', versionId: '4', links: ['2', '3'], updatedAt: '' },
+  { docId: 'A', versionId: '5', links: ['4'], updatedAt: '' },
+  { docId: 'A', versionId: '6', links: ['4'], updatedAt: '' },
+  { docId: 'A', versionId: '7', links: ['4'], updatedAt: '' },
+  { docId: 'A', versionId: '8', links: ['5', '6'], updatedAt: '' },
 ]
 
 const scenarios = [
   {
     docs: docs.slice(0, 2),
-    expected: { id: 'A', version: '2', links: ['1'], forks: [] },
+    expected: {
+      docId: 'A',
+      versionId: '2',
+      links: ['1'],
+      forks: [],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 3),
-    expected: { id: 'A', version: '3', links: ['1'], forks: ['2'] },
+    expected: {
+      docId: 'A',
+      versionId: '3',
+      links: ['1'],
+      forks: ['2'],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 4),
-    expected: { id: 'A', version: '4', links: ['2', '3'], forks: [] },
+    expected: {
+      docId: 'A',
+      versionId: '4',
+      links: ['2', '3'],
+      forks: [],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 5),
-    expected: { id: 'A', version: '5', links: ['4'], forks: [] },
+    expected: {
+      docId: 'A',
+      versionId: '5',
+      links: ['4'],
+      forks: [],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 6),
-    expected: { id: 'A', version: '6', links: ['4'], forks: ['5'] },
+    expected: {
+      docId: 'A',
+      versionId: '6',
+      links: ['4'],
+      forks: ['5'],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 7),
-    expected: { id: 'A', version: '7', links: ['4'], forks: ['5', '6'] },
+    expected: {
+      docId: 'A',
+      versionId: '7',
+      links: ['4'],
+      forks: ['5', '6'],
+      updatedAt: '',
+    },
   },
   {
     docs: docs.slice(0, 8),
-    expected: { id: 'A', version: '8', links: ['5', '6'], forks: ['7'] },
+    expected: {
+      docId: 'A',
+      versionId: '8',
+      links: ['5', '6'],
+      forks: ['7'],
+      updatedAt: '',
+    },
   },
 ]
 
@@ -53,11 +95,11 @@ test('Expected head for all permutations of order', async (t) => {
     // for (const permutation of [[docs[0], docs[3], docs[1], docs[2]]]) {
     for (const permutation of permute(docs)) {
       indexer.batch(permutation)
-      const head = api.getDoc(expected.id)
+      const head = api.getDoc(expected.docId)
       t.deepEqual(
         { ...head, forks: head?.forks.sort() },
         expected,
-        JSON.stringify(permutation.map((doc) => doc.version))
+        JSON.stringify(permutation.map((doc) => doc.versionId))
       )
       clear()
     }
